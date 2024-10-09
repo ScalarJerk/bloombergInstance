24ab693d6f9960cd1cba257a72c9286eb424d269
Sudibyo Sinha <88853543+ScalarJerk@users.noreply.github.com>
2024-10-09 19:16:47 +0000

    Added SDK kit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://data.bloomberglp.com/professional/sites/10/2017/03/BLPAPI-Core-User-Guide.pdf
+https://data.bloomberglp.com/professional/sites/10/2017/03/BLPAPI-Core-Developer-Guide.pdf
+https://www.bloomberg.com/professional/support/api-library/
---
+++ blpapi_cpp_3.24.8.1/doc/html/navtreedata.js
@@ -0,0 +1,82 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "BLPAPI C++", "index.html", [
+    [ "Main Page", "index.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"blpapi__element_8h.html#a008dd99e97c146dca6c1ca8a8a2b9e82",
+"blpapi__providersession_8h.html#a2a7c29f642391df54232c8e92a425a42",
+"blpapi__subscriptionlist_8h.html#a00b21c3668a8ee32f28a77f5f81fa3f3",
+"classBloombergLP_1_1blpapi_1_1CorrelationId.html#ad9971b6ef33e02ba2c75d19c1d2518a1acd1ce0b084595a6072a57781dc7738a0",
+"classBloombergLP_1_1blpapi_1_1EventFormatter.html#a12ab8a4ce35956aa4db122072dae0c4a",
+"classBloombergLP_1_1blpapi_1_1ProviderSession.html#abe98af4fb5d9e9b89b65a191b46c5da8",
+"classBloombergLP_1_1blpapi_1_1SessionOptions.html#a2e8ca794e5601dab2a9407a38822270e",
+"functions_g.html",
+"group__blpapi__types.html#ggae51aef6b8fe12b4657827355e5a99531a6d6f3ee2b33ae661fc620b7a42dde89d",
+"structblpapi__FunctionEntries.html#a1d6eb020e44ca4e89fac67a30fd7e665"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
